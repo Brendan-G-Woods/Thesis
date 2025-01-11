6920c78f96fa89c1788c5aed553ccb142b876b00
Brendan Woods <B.Woods6@universityofgalway.ie>
2025-01-11 18:23:30 +0000

    P-value calculation, and removal of unnceccesary code relating to mean calculations

--- Sample Code.R
+++ Sample Code.R
@@ -4,19 +4,19 @@ library(dplyr)
 load("\\Users\\brend\\Downloads\\data")
 ls
 View(tmpDat)
-thesis.df<-tmpDat
+## Creating binary column for Insulin ##
+tmpDat$binary_insulin<-0
+tmpDat$binary_insulin[thesis.df$InsulinYN == "Yes"]<-1
 
-Metformin_Insulin_proportion<-sum(tmpDat$InsulinYN[tmpDat$Group== "Metformin"] == "Yes", na.rm=T)/sum(tmpDat$Group == "Metformin")
-Metformin_gw32_mean<-mean(tmpDat$gw32[tmpDat$Group == "Metformin"], na.rm=T)
-Metformin_gw38_mean<-mean(tmpDat$gw38[tmpDat$Group == "Metformin"], na.rm=T)
+thesis.df<-tmpDat
 
 ## T-values and Z-scores for the original study ##
 StudyInsulinTest<- prop.test(x = c(sum(tmpDat$InsulinYN[tmpDat$Group== "Metformin"] == "Yes", na.rm=T),
                               sum(tmpDat$InsulinYN[tmpDat$Group== "Placebo"] == "Yes", na.rm=T)),
                         n = c(sum(tmpDat$Group == "Metformin"),sum(tmpDat$Group == "Placebo")))
-z_score_squared_insulin<-sqrt(StudyInsulinTest$statistic)                              
+z_score_squared_insulin<-sqrt(StudyInsulinTest$statistic)       ## This is not used, binary insulin more appropriate to use ##                       
 
-
+insulin_t_value<-t.test(tmpDat$binary_insulin[tmpDat$Group == "Metformin"], tmpDat$binary_insulin[tmpDat$Group == "Placebo"])$statistic
 gw32_t_value<-t.test(tmpDat$gw32[tmpDat$Group == "Metformin"], tmpDat$gw32[tmpDat$Group == "Placebo"])$statistic
 gw38_t_value<-t.test(tmpDat$gw38[tmpDat$Group == "Metformin"], tmpDat$gw38[tmpDat$Group == "Placebo"])$statistic
 
@@ -25,26 +25,6 @@ gw38_t_value<-t.test(tmpDat$gw38[tmpDat$Group == "Metformin"], tmpDat$gw38[tmpDa
 study_squared_composite<-z_score_squared_insulin+(gw32_t_value^2)+(gw38_t_value^2)
 ##Not squared##
 study_non_squared_composite<-sqrt(z_score_squared_insulin)+gw32_t_value+gw38_t_value
-
-
-## Previous permutation codes using mean ##
-permutation_function<-function(x){
-  permutation_result<-numeric(10000)
-  i <-1
-  repeat{
-    thesis.df$Group<-sample(thesis.df$Group, 535, replace = F)
-    permutation_result[i]<-mean(x[thesis.df$Group == "Metformin"], na.rm=T) 
-    i<- i +1
-    if(i > 10000) break}
-  return(permutation_result)
-}
-summary(permutation_function(thesis.df$gw32))
-hist(permutation_function(thesis.df$gw32), breaks = 100, main= "Histogram of Permuted Means of Fasting Glucose Level at Week 32", xlab= "Mean")
-summary(permutation_function(thesis.df$gw38))
-hist(permutation_function(thesis.df$gw38), breaks = 100, main= "Histogram of Permuted Means of Fasting Glucose Level at Week 38", xlab= "Mean")
-InsulinBinary<-ifelse(thesis.df$InsulinYN == "Yes", 1, 0)
-summary(permutation_function(InsulinBinary))
-hist(permutation_function(InsulinBinary), breaks = 100, main= "Histogram of Permuted Mean Insulin", xlab= "Proportion Insulin")
 
 ##Permutation Code for t-values for continuous data (gw32, gw38) and for z-score for binary data (InsulinYN) ##
 
@@ -95,12 +75,27 @@ hist(continuous_permutation_function(thesis.df$gw38),
 Zi+t32+t38
 ## The squared method ##
 
-squared_composite<-proportion_permutation_function(thesis.df$InsulinYN) +
+squared_composite<-((continuous_permutation_function(thesis.df$binary_insulin))^2) +
   ((continuous_permutation_function(thesis.df$gw32))^2) +
   ((continuous_permutation_function(thesis.df$gw38))^2)
 
 ## Non-squared method ##
-non_squared_composite<-sqrt(proportion_permutation_function(thesis.df$InsulinYN)) +
+non_squared_composite<-continuous_permutation_function(thesis.df$binary_insulin) +
   continuous_permutation_function(thesis.df$gw32) +
   continuous_permutation_function(thesis.df$gw38)
 
+##P-value calculation##
+
+##Individual variables##
+p_value_gw32<-sum(continuous_permutation_function(thesis.df$gw32)<gw32_t_value)/
+  length(continuous_permutation_function(thesis.df$gw32))
+p_value_gw38<-sum(continuous_permutation_function(thesis.df$gw38)<gw38_t_value)/
+  length(continuous_permutation_function(thesis.df$gw38))
+p_value_binary_insulin<-sum(continuous_permutation_function(thesis.df$binary_insulin)<insulin_t_value)/
+  length(continuous_permutation_function(thesis.df$binary_insulin))
+
+##Composite P-values##
+p_value_squared_composite<-sum(squared_composite>study_squared_composite)/
+  length(squared_composite)
+p_value_non_squared_composite<-sum(non_squared_composite<study_non_squared_composite)/
+  length(non_squared_composite)
